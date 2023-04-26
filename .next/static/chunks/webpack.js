78d688ed3d533836e8834569d545b540ff118339
Decago <deca@DeveNinja.local>
2023-04-26 22:56:28 +0100

    fixed profile pic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8dc6a42160c28fb0"; }
+/******/ 		__webpack_require__.h = function() { return "a107a74276d024ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import userStyle from '../styles/userprofile.module.css'
-import {BsHexagon} from 'react-icons/Bs'
+import {FiHexagon} from 'react-icons/Fi'
 import Image from 'next/image';
 import { userAgent } from 'next/server';
 
@@ -13,7 +13,7 @@ const UserProfile: React.FC<profile> =({data, image}) => {
   return (
         <main>
             <div className={userStyle.container}>
-                <BsHexagon className={userStyle.hexagon} />
+                <FiHexagon className={userStyle.hexagon} />
                 <img src={image !== undefined ? image : "User doesn't exist"} alt = "profile-pic" width={80} height={80} id={userStyle.img}/>
                 <div>
                     <center>
