f09195e233e98930635eefd5ff235ef121dcb491
Decago <deca@DeveNinja.local>
2023-04-26 23:13:00 +0100

    react icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a107a74276d024ab"; }
+/******/ 		__webpack_require__.h = function() { return "eca9fed5edec5972"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.1ee5a32ad583ef9d.hot-update.js
+++ .next/static/webpack/webpack.1ee5a32ad583ef9d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "31b57726b422153a"; }
+/******/ 	__webpack_require__.h = function() { return "79c3a6f47a5da92f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
